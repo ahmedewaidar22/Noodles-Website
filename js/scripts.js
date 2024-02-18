2336a0b154ac36e2abfad447564c8f357685713e
ahmedewaidar22 <ahmedewaidar22@gmail.com>
2024-02-18 20:31:36 +0200

    add website

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+
+      (function($) {
+    "use strict"; 
+    $('.js-scroll-trigger').click(function() {
+      $('.navbar-collapse').collapse('hide');
+    });
+
+    $('body').scrollspy({
+      target: '#mainNav',
+      offset: 75
+    });
+
+    var navbarCollapse = function() {
+      if ($("#mainNav").offset().top > 100) {
+        $("#mainNav").addClass("navbar-scrolled");
+      } else {
+        $("#mainNav").removeClass("navbar-scrolled");
+      }
+    };
+    navbarCollapse();
+    $(window).scroll(navbarCollapse);
+  })(jQuery); // End of use strict
